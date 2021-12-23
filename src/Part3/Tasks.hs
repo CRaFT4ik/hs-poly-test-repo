5abb168a4c8fe8e8a6df040ec348ec55d4391eaa
Eldar <eldar.tim@gmail.com>
2021-12-23 03:34:00 +0300

    Added solution for Part 3

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,25 +1,34 @@
 module Part3.Tasks where
 
 import Util (notImplementedYet)
+import Data.List (nubBy)
 
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
-finc = notImplementedYet
+finc f n = f n : finc f (n + 1)
 
 -- Функция ff принимает на вход функцию f и элемент x и возвращает список [x, f(x), f(f(x)), f(f(f(x))) ...]
 ff :: (a -> a) -> a -> [a]
-ff = notImplementedYet
+ff f x = x : ff f (f x)
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq list = notImplementedYet -- map ( next)
+
+-- comp :: [Int] -> (Int, Int)
+-- comp list = let h = head list
+--    in length $ filter (== h) list
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
-uniq = notImplementedYet
+uniq [] = []
+uniq (h : t) = h : uniq (filter (h /=) t)
 
 -- Функция grokBy принимает на вход список Lst и функцию F и каждому возможному
 -- значению результата применения F к элементам Lst ставит в соответствие список элементов Lst,
 -- приводящих к этому результату. Результат следует представить в виде списка пар.
 grokBy :: (Eq k) => (a -> k) -> [a] -> [(k, [a])]
-grokBy f l = notImplementedYet
+grokBy f l =
+  let fls = map (\e -> (e, f e)) l
+      uniqFls = nubBy (\x y -> snd x == snd y) fls
+   in map (\e -> (snd e, map fst $ filter (\(_, k2) -> k2 == snd e) fls)) uniqFls
